1492d6bb49bbaefe43001545971bf9bdab632fb3
houzw <houzw@allinfinance.com>
2019-12-30 10:31:06 +0800

    数组结构-ngx_array

--- src/core/ngx_array.c
+++ src/core/ngx_array.c
@@ -8,17 +8,18 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+// ####### 创建数组，p：内存池、n：数组支持元素个数、size：每个元素的大小
 ngx_array_t *
 ngx_array_create(ngx_pool_t *p, ngx_uint_t n, size_t size)
 {
     ngx_array_t *a;
 
+    // 从内存池中分配一段内存给数组的数据结构
     a = ngx_palloc(p, sizeof(ngx_array_t));
     if (a == NULL) {
         return NULL;
     }
-
+    // 调用数组初始化函数
     if (ngx_array_init(a, p, n, size) != NGX_OK) {
         return NULL;
     }
@@ -26,24 +27,27 @@ ngx_array_create(ngx_pool_t *p, ngx_uint_t n, size_t size)
     return a;
 }
 
-
+// ####### 数组销毁 ？？FIXME当数组尾部不是内存池可用位置时怎么办？？？？
 void
 ngx_array_destroy(ngx_array_t *a)
 {
     ngx_pool_t  *p;
 
     p = a->pool;
-
+    // 若数组元素末尾地址和内存池开始可用地址相同，
+    // 则将内存池可用位置前移到数组元素开始位置
     if ((u_char *) a->elts + a->size * a->nalloc == p->d.last) {
         p->d.last -= a->size * a->nalloc;
     }
 
+    // 清理数组数据结构，若数组数据结构末尾和内存池可用位置相同，
+    // 则将内存池可用位置前移到数组结构开始位置
     if ((u_char *) a + sizeof(ngx_array_t) == p->d.last) {
         p->d.last = (u_char *) a;
     }
 }
 
-
+// ####### 数组添加一个元素
 void *
 ngx_array_push(ngx_array_t *a)
 {
@@ -62,6 +66,7 @@ ngx_array_push(ngx_array_t *a)
         if ((u_char *) a->elts + size == p->d.last
             && p->d.last + a->size <= p->d.end)
         {
+            // ==当前内存池可用大小足以分配请求大小时==
             /*
              * the array allocation is the last in the pool
              * and there is space for new allocation
@@ -71,13 +76,15 @@ ngx_array_push(ngx_array_t *a)
             a->nalloc++;
 
         } else {
-            /* allocate a new array */
+            // ==当前内存池可用大小不足以分配请求大小时==
 
+            /* allocate a new array */
+            // 申请元素组两倍长度的新数组
             new = ngx_palloc(p, 2 * size);
             if (new == NULL) {
                 return NULL;
             }
-
+            // 拷贝原数组内容到新数组
             ngx_memcpy(new, a->elts, size);
             a->elts = new;
             a->nalloc *= 2;
@@ -90,7 +97,7 @@ ngx_array_push(ngx_array_t *a)
     return elt;
 }
 
-
+// ####### 数组添加n个元素
 void *
 ngx_array_push_n(ngx_array_t *a, ngx_uint_t n)
 {
@@ -99,8 +106,9 @@ ngx_array_push_n(ngx_array_t *a, ngx_uint_t n)
     ngx_uint_t   nalloc;
     ngx_pool_t  *p;
 
-    size = n * a->size;
+    size = n * a->size; // 计算出分配总大小
 
+    // 数组容量不足时，扩容数组
     if (a->nelts + n > a->nalloc) {
 
         /* the array is full */
@@ -120,7 +128,7 @@ ngx_array_push_n(ngx_array_t *a, ngx_uint_t n)
 
         } else {
             /* allocate a new array */
-
+            // 申请元素组两倍长度的新数组
             nalloc = 2 * ((n >= a->nalloc) ? n : a->nalloc);
 
             new = ngx_palloc(p, nalloc * a->size);
@@ -128,12 +136,14 @@ ngx_array_push_n(ngx_array_t *a, ngx_uint_t n)
                 return NULL;
             }
 
+            // 拷贝原数组内容到新数组
             ngx_memcpy(new, a->elts, a->nelts * a->size);
             a->elts = new;
             a->nalloc = nalloc;
         }
     }
 
+    // 数组容量充足时，直接分配
     elt = (u_char *) a->elts + a->size * a->nelts;
     a->nelts += n;
 
--- src/core/ngx_array.h
+++ src/core/ngx_array.h
@@ -12,22 +12,25 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+// ####### 数组Array结构定义
 typedef struct {
-    void        *elts;
-    ngx_uint_t   nelts;
-    size_t       size;
-    ngx_uint_t   nalloc;
-    ngx_pool_t  *pool;
+    void        *elts;  // 指向数组第一个元素的指针
+    ngx_uint_t   nelts; // 未使用元素的索引
+    size_t       size;  // 每个元素的大小，数组创建后就固定死了
+    ngx_uint_t   nalloc;// 一共分配了多少个元素(即当前数组的容量)，若不够用，会自动扩容
+    ngx_pool_t  *pool;  // 内存池
 } ngx_array_t;
 
-
+// 创建数组
 ngx_array_t *ngx_array_create(ngx_pool_t *p, ngx_uint_t n, size_t size);
+// 销毁数组
 void ngx_array_destroy(ngx_array_t *a);
+// 添加一个元素
 void *ngx_array_push(ngx_array_t *a);
+// 添加多个元素
 void *ngx_array_push_n(ngx_array_t *a, ngx_uint_t n);
 
-
+// 创建数组函数调用该函数，初始化数组
 static ngx_inline ngx_int_t
 ngx_array_init(ngx_array_t *array, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
@@ -41,6 +44,7 @@ ngx_array_init(ngx_array_t *array, ngx_pool_t *pool, ngx_uint_t n, size_t size)
     array->nalloc = n;
     array->pool = pool;
 
+    // 从内存池中分配n*size大小的内存给数组，数组第一个元素指针指向这片内存
     array->elts = ngx_palloc(pool, n * size);
     if (array->elts == NULL) {
         return NGX_ERROR;
